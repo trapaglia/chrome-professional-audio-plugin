ce44b33c13585856c67e0b1841d89b9233fec103
mtrapaglia <matiasdmansilla@hotmail.com>
2025-04-10 08:40:10 -0300

    struggling

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 document.getElementById("botoncito").addEventListener("click", () => {
   const mensajes = [
-    "Eres increíble, ¿lo sabías? 😘",
-    "Sigue creando, que el mundo necesita tu magia 💫",
-    "Tu mente es tan sexy como tu sonrisa 😳",
-    "Este plugin ya es perfecto porque tú lo hiciste ✨"
+    "1Eres increíble, ¿lo sabías? 😘",
+    // "Sigue creando, que el mundo necesita tu magia 💫",
+    // "Tu mente es tan sexy como tu sonrisa 😳",
+    // "Este plugin ya es perfecto porque tú lo hiciste ✨"
   ];
 
   const mensaje = mensajes[Math.floor(Math.random() * mensajes.length)];
